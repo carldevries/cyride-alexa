1792853c2a94566ba170ec20342626b328c34a52
Carl De Vries <carldevries@gmail.com>
2017-05-05 16:37:27 -0500

    Added dist folder, MapQuest test properties, and Amazon test properties files to .gitignore. Updated codestyle.bat to only perform code style checks on Python files in the src directory (avoid dist\app).

--- codestyle.bat
+++ codestyle.bat
@@ -3,7 +3,7 @@
 if not exist logs\ mkdir logs
 if exist logs\pep8.log rm logs\pep8.log
 
-for /r %%d in (*.py) do (
+for /r src %%d in (*.py) do (
     echo %%d >> logs\pep8.log
     echo. >> logs\pep8.log
     pep8 --show-source --show-pep8 %%d >> logs\pep8.log
